6466f239d22feda0ca998a5675185cc5af5f9b98
chenxiaoangle <chenxiaoangle@gmail.com>
2017-03-24 15:24:58 +0800

    add some writehead thing

--- app/index.js
+++ app/index.js
@@ -12,6 +12,7 @@ class App {
             let { url } = request;
             //每个请求逻辑 根据url进行分发
             let body = staticServer(url)
+            response.writeHead(200,'resolve ok',{'X-powered-by':'node.js'})
             response.end(body)
         }
     }
