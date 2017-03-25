2e431c4121ae255fb66889417c0c310306f2e198
tomchou <tomchou@tomchoudeMacBook-Pro.local>
2017-03-25 22:50:39 +0800

    learn some Promise apis

--- test.js
+++ test.js
@@ -15,10 +15,15 @@
 //first step: new Promise
 
 let p = new Promise((resolve,reject)=>{
-    resolve(1)
-});
+        setTimeout(reject,1000,'hello world')
+    });
 console.log(p)
-
-p.then(val=>{
-    console.log(`then val is ${val}`)
-})
+//then has two premeters , first is dealing with resolve,second is dealing with reject
+p.then(val=>
+    //deal with resolve
+    console.log(`resolve then val is ${val}`),
+        val=>
+        //deal with reject
+        console.log(`reject val is ${val}`)
+);
+setTimeout(()=>p.catch(val=>console.log(`another val is ${val}`)),2000)
